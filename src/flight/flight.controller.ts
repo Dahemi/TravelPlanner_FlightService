d0e757faaccef5f510b883b51c997f66ee760f27
Dahami <126935880+Dahemi@users.noreply.github.com>
2025-10-16 15:55:14 +0530

    date filtering added

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -1,25 +1,25 @@
-import { Controller, Get,Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { FlightService } from './flight.service';
 
 @Controller('flights')
 export class FlightController {
+  constructor(private readonly flightService: FlightService) {}
 
-    constructor(private readonly flightService: FlightService){}
+  @Get('search')
+  async searchFlights(
+    @Query('from') from: string,
+    @Query('to') to: string,
+    @Query('date') date: string,
+  ) {
+    return this.flightService.searchFlights(from, to, date);
+  }
 
-    @Get('search')
-    async searchFlights(
-        @Query('from') from:string,
-        @Query('to') to:string,
-
-    ){
-        return this.flightService.searchFlights(from,to);
-    }
-
-    @Get('cheapest')
-    async cheapestFlight(
-        @Query('from') from:string,
-        @Query('to') to:string,
-    ){
-        return this.flightService.cheapestFlight(from,to);
-    }
+  @Get('cheapest')
+  async cheapestFlight(
+    @Query('from') from: string,
+    @Query('to') to: string,
+    @Query('date') date: string,
+  ) {
+    return this.flightService.cheapestFlight(from, to, date);
+  }
 }
--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -2,27 +2,52 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class FlightService {
-    private flights = [
-    { id: 1, from: 'CMB', to: 'BKK', departTime: '2025-12-10T09:00', arriveTime: '2025-12-10T13:00', price: 450 },
-    { id: 2, from: 'CMB', to: 'BKK', departTime: '2025-12-10T15:00', arriveTime: '2025-12-10T21:00', price: 400 },
-    { id: 3, from: 'CMB', to: 'SIN', departTime: '2025-12-10T12:00', arriveTime: '2025-12-10T16:00', price: 350 },
+  private flights = [
+    {
+      id: 1,
+      from: 'CMB',
+      to: 'BKK',
+      departTime: '2025-12-11T09:00',
+      arriveTime: '2025-12-11T13:00',
+      price: 450,
+    },
+    {
+      id: 2,
+      from: 'CMB',
+      to: 'BKK',
+      departTime: '2025-12-10T15:00',
+      arriveTime: '2025-12-10T21:00',
+      price: 400,
+    },
+    {
+      id: 3,
+      from: 'CMB',
+      to: 'SIN',
+      departTime: '2025-12-12T12:00',
+      arriveTime: '2025-12-12T16:00',
+      price: 350,
+    },
   ];
 
-  async searchFlights(from: string, to: string){
+  async searchFlights(from: string, to: string, date: string) {
     const delay = 800;
-    await new Promise(res => setTimeout(res,delay));
+    await new Promise((res) => setTimeout(res, delay));
 
-    const results = this.flights.filter(f => f.from == from && f.to == to);
-    return{flights:results};
+    const results = this.flights.filter(
+      (f) => f.from == from && f.to == to && f.departTime.startsWith(date),
+    );
+    return { flights: results };
   }
 
-  async cheapestFlight(from:string, to:string){
+  async cheapestFlight(from: string, to: string, date: string) {
     const delay = 800;
-    await new Promise(res => setTimeout(res,delay));
+    await new Promise((res) => setTimeout(res, delay));
 
-    const results = this.flights.filter(f => f.from == from && f.to == to);
-    if(results.length == 0) return {flight: null};
-    const cheapest = results.reduce((a,b) => (a.price <= b.price ? a: b));
-    return {flight:cheapest};
+    const results = this.flights.filter(
+      (f) => f.from == from && f.to == to && f.departTime.startsWith(date),
+    );
+    if (results.length == 0) return { flight: null };
+    const cheapest = results.reduce((a, b) => (a.price <= b.price ? a : b));
+    return { flight: cheapest };
   }
 }
